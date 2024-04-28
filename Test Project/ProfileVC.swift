1bb9657aacdee6b55fc1996054a1a55e5d0e9bd0
VasoBaramidze824 <vasilbaramidze2228@gmail.com>
2024-04-28 15:16:15 +0200

    remove vc lifecycle functions

--- Test Project/ProfileVC.swift
+++ Test Project/ProfileVC.swift
@@ -22,20 +22,6 @@ class ProfileVC: UIViewController {
         fdnblfdnb.textColor = UIColor.red
         numberGenerateBtn.setTitleColor(.purple, for: .normal)
     }
-    override func viewWillAppear(_ animated: Bool) {
-        super.viewWillAppear(animated)
-        
-    }
-    
-    override func viewDidAppear(_ animated: Bool) {
-        super.viewDidAppear(animated)
-        
-    }
-    
-    override func viewWillDisappear(_ animated: Bool) {
-        super.viewWillDisappear(animated)
-        
-    }
     
     @IBAction func generateNumber(_ sender: Any) {
         fdnblfdnb.text = "1"
